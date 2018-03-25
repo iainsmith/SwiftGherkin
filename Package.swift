999b287462e5f2d19a67053225cd714ebe20aef5
Iain <iain@mountain23.com>
2018-03-25 17:44:17 +0100

    Add consumer

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
             targets: ["SwiftGherkin"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/nicklockwood/Consumer", from: "0.3.2"),
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
