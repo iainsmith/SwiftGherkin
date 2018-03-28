0e82816784e839b509e9b8b2b75b651537c1b58e
Iain <iain@mountain23.com>
2018-03-28 19:21:43 +0100

    Update to Consumer 0.3.3

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["Gherkin"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/iainsmith/Consumer.git", .branch("master")),
+        .package(url: "https://github.com/nicklockwood/Consumer.git", .upToNextMinor(from: "0.3.3")),
     ],
     targets: [
         .target(
